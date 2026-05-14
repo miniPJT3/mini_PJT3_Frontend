669fb72df63328cf23942de8583f1316324b2f2c
hongjiho5148 <wlgh51485148@gmail,com>
2026-05-14 18:46:50 +0900

    Revert "권한 없는 페이지 접근 제어"
    This reverts commit e9107fb031d02c39f9181e7e92be2269e69107e8.

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
   server: {
     proxy: {
       // 일반 API 요청을 위한 설정
